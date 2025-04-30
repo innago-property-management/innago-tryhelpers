42f329e5ac71100b48aac0ff9d1e160f52d0bc29
chris-innago <chris@innago.com>
2025-04-30 09:11:23 -0400

    Add UnitTests project to solution

---
+++ tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTests;
+
+using FluentAssertions;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        const string expected = "the dev was NOT too lazy to write tests";
+        const string actual = "the dev was too lazy to write tests";
+
+        actual.Should().NotBe(expected);
+    }
+}
